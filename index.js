85864391b5a3fe365fb97e4127fb3fd10536c4ba
Aurorn <aurornispro@gmail.com>
2024-06-21 16:41:31 -0400

    added prepend to be printed

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 import LinkedList from './LinkedList.js';
 
 const list = new LinkedList();
+
+list.append(2);
+list.append(3);
+
+list.prepend(1);
+
+function displayList(list) {
+    let current = list.headNode;
+    let result = '';
+    while (current) {
+      result += `( ${current.value} ) -> `;
+      current = current.nextNode;
+    }
+    result += 'null';
+    return result;
+  }
+
+  console.log(displayList(list));
+  console.log('List size:', list.length);
